e65944070724dd014851619cc858b3950543c1ac
303729223@qq.com <yong6457493>
2025-12-29 13:44:03 +0800

    修改跨域问题

--- app/extensions.py
+++ app/extensions.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 # 初始化数据库
 db = SQLAlchemy()
 migrate = Migrate()
-cors = CORS()
+cors = CORS(supports_credentials=True)  # 支持凭据，允许跨域访问
 
 # def init_extensions(app):
 #     db.init_app(app)  # 将数据库连接绑定到Flask app
